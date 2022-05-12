f7c719074a28e994b4cf564a8fc5779c5e57b695
Deskuma <deskuma@gmail.com>
2022-05-13 07:39:05 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='nDice',
+      version='1.0.1',
+      description='nDn Dice for Python',
+      author='Deskuma',
+      url='https://github.com/Deskuma/ndn-dice',
+      packages=['ndice'],
+      license='MIT',
+      )
